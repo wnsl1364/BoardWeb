c37ebed52d3fa5f79e697ffbd4cdbf5358732aab
admin <admin@DESKTOP-NNU1EL4>
2025-04-02 17:47:39 +0900

    !!!!!!!!

--- BoardWeb/src/main/java/com/yedam/common/Control.java
+++ BoardWeb/src/main/java/com/yedam/common/Control.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//interface를 만드는 이유: 컨트롤이 가진 공통된 메소드 exec실행을 위해서
 public interface Control {
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
 }
--- BoardWeb/src/main/java/com/yedam/common/FrontController.java
+++ BoardWeb/src/main/java/com/yedam/common/FrontController.java
@@ -23,14 +23,15 @@ import com.yedam.control.MainControl;
 
 public class FrontController extends HttpServlet {
 
-	// 요청 url <=>실행컨트롤
+	// Map: 요청 url => 실행할 컨트롤 매핑
 	Map<String, Control> map;
 
 	// 생성자
 	public FrontController() {
-		map = new HashMap<String, Control>();
+		map = new HashMap<String, Control>(); //HashMap: 순서없이 키를 이용해 값을 빠르게 꺼낼 수 있음
 	}
 
+	//init() 메서드: 맵핑 설정
 	@Override
 	public void init() throws ServletException {
 		map.put("/main.do", new MainControl()); // 메인화면
@@ -46,15 +47,31 @@ public class FrontController extends HttpServlet {
 		map.put("/logout.do", new LogoutControl());// 로그아웃 처리
 	}
 
+	//service() 메서드: 요청처리 핵심
 	@Override
+	// HttpServlet이 받는 모든 요청을 처리하는 메서드
+	// 브라우저에서 .do로 끝나는 요청이 들어오면 이 메서드가 호출됨
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String uri = req.getRequestURI();
-		System.out.println("uri: " + uri);
-		String context = req.getContextPath();
-		System.out.println("context: " + context);
-		String page = uri.substring(context.length());
-		System.out.println("page: " + page);
-		Control sub = map.get(page);
-		sub.exec(req, resp);
+	    
+	    // 1. 전체 요청 URI를 가져옴 (예: /BoardWeb/boardList.do)
+	    String uri = req.getRequestURI(); 
+	    System.out.println("uri: " + uri); 
+
+	    // 2. 프로젝트 컨텍스트 경로를 가져옴 (예: /BoardWeb)
+	    String context = req.getContextPath(); 
+	    System.out.println("context: " + context); 
+
+	    // 3. context 길이 이후의 경로만 잘라서 가져옴 (예: /boardList.do)
+	    //    이게 map의 key 값으로 사용됨
+	    String page = uri.substring(context.length()); 
+	    System.out.println("page: " + page); 
+
+	    // 4. 요청한 경로(page)를 기준으로 Control 객체를 찾음
+	    //    예: "/boardList.do" => new BoardListControl()
+	    Control sub = map.get(page); 
+
+	    // 5. 해당 Control 객체의 exec() 메서드를 실행해서
+	    //    실제 비즈니스 로직(DB 처리 등)을 수행하게 함
+	    sub.exec(req, resp); 
 	}
 }
